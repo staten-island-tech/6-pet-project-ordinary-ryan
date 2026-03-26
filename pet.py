300276195cfbe466e3685e7cd3db447793a8b054
ordinary-ryan <ryanl422@nycstudents.net>
2026-03-26 10:16:18 -0400

    pet ig

--- pet.py
+++ pet.py
@@ -1,16 +1,30 @@
-
-
-
-class Hero:
-    def __init__(self, name, money, inventory):
+class Classpet:
+    def __init__(self, name, money, inventory, happiness):
         self.name = name
         self.money = money
         self.inventory = inventory
-
-    def buy(self, item):
+        self.happiness = happiness
+Boomy = Classpet("Boomy", 500, ["Boots"], 100)
+print(Boomy.__dict__) 
+"""     def buy(self, item):
         self.inventory.append(item)
-        print(self.inventory)
-    
-Ixia = Hero("Ixia", 500, ["Boots"])
-Ixia.buy({"title": "Sword", "atk": 34})
-print(Ixia.__dict__)
+        print(self.name, self.money, self.inventory) """
+action = input("what do you want to do: compliment, make a joke, laugh at, attack ")
+def happy(self, joy):
+    if action == "compliment":
+        joy = 50
+    elif action == "make a joke":
+        joy = 25
+    elif action == "attack":
+        joy = -100
+    elif happiness == "laugh at":
+        joy = -50
+
+    happiness += joy
+    if happiness >= 100:
+        self.happiness = 'happy'
+    else:
+        self.happiness = 'sad'
+    print("Boomy's is now", self.happiness,)
+
+""" Boomy.buy({"title": "Sword", "atk": 34}) """

6aed0470ce4aad4beae880480a158d0bcf545442
ordinary-ryan <ryanl422@nycstudents.net>
2026-03-27 10:17:10 -0400

    idk

--- pet.py
+++ pet.py
@@ -6,9 +6,9 @@ class Classpet:
         self.happiness = happiness
 Boomy = Classpet("Boomy", 500, ["Boots"], 100)
 print(Boomy.__dict__) 
-"""     def buy(self, item):
-        self.inventory.append(item)
-        print(self.name, self.money, self.inventory) """
+""" def buy(self, item):
+    self.inventory.append(item)
+    print(self.name, self.money, self.inventory) """
 action = input("what do you want to do: compliment, make a joke, laugh at, attack ")
 def happy(self, joy):
     if action == "compliment":
@@ -17,14 +17,14 @@ def happy(self, joy):
         joy = 25
     elif action == "attack":
         joy = -100
-    elif happiness == "laugh at":
+    elif action == "laugh at":
         joy = -50
 
-    happiness += joy
-    if happiness >= 100:
-        self.happiness = 'happy'
+    self += joy
+    if self >= 100:
+        self = 'happy'
     else:
-        self.happiness = 'sad'
-    print("Boomy's is now", self.happiness,)
+        self = 'sad'
+    print("Boomy's is now", self)
 
 """ Boomy.buy({"title": "Sword", "atk": 34}) """

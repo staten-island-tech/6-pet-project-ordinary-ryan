4a0ac4745ca23b35934f7fef7643f68e9b50b41e
ordinary-ryan <ryanl422@nycstudents.net>
2026-04-17 10:03:23 -0400

    Update pet.py

--- pet.py
+++ pet.py
@@ -1,41 +1,57 @@
 class Classpet:
-    def __init__(self, name, happiness):
+    def __init__(self, name, happiness, hunger):
         self.name = name
         self.happiness = happiness
+        self.hunger = hunger
 
-Boomy = Classpet("Boomy", 100 )
+Boomy = Classpet("Boomy", 100, 100)
 print(Boomy.__dict__) 
 
-def show_balance(self):
-    print(f"{self.name} happiness is now{self.happiness}")
-
 def happy(self):
-    action = input("what do you want to do: compliment, make a joke, laugh at, attack ")
+    action = input("what do you want to do: compliment, feed, attack, pet")
     cont = True
     joy = 0
+    hungry = 0
     while cont == True:
         if action == "compliment":
             joy = 50
-        elif action == "make a joke":
+            hungry = -10
+        elif action == "feed":
             joy = 25
+            hungry = 50
         elif action == "attack":
             joy = -100
-        elif action == "laugh at":
+            hungry = -50
+        elif action == "pet":
             joy= -50
+            hungry =-10
     
         self.happiness += joy
-
-        if self.happiness >= 100:
-            print(self.name, "is happy")
-        else:
-            print(self.name, "is sad")
+        self.hunger += hungry
 
         cont_ = input("do you want to continue: yes or no")
         if cont_ == "yes":
             cont = True
+            action = input("what do you want to do: compliment, feed, attack, pet")
         elif cont_ == "no":
             cont = False
-            break
+            break 
+        
+        
+
+    if self.happiness <= 0 or self.hunger <= 0:
+        print(self.name, "is dead")
+    elif self.happiness >= 0 and self.happiness <= 50: 
+        print(self.name, "is sad")
+    else:
+        print(self.name, "is happy")
+    
+    if self.hunger >= 0 and self.hunger <= 50: 
+        print(self.name, "is hungry")
+    else:
+        print(self.name, "is full")
+
+
     print("Thank you for playing")
 happy(Boomy)
 

03f8fea9220568006c5432b1d7ae294f02d18bc9
ordinary-ryan <ryanl422@nycstudents.net>
2026-04-20 10:17:23 -0400

    Update pet.py

--- pet.py
+++ pet.py
@@ -14,17 +14,21 @@ def happy(self):
     hungry = 0
     while cont == True:
         if action == "compliment":
-            joy = 50
-            hungry = -10
+            joy += 50
+            hungry += -10
+            print('Boomy is delighted by your compliment: happiness + 50, hunger -10')
         elif action == "feed":
-            joy = 20
-            hungry = 35
+            joy += 20
+            hungry += 35
+            print('Boomy is stuffed: happiness +20, hunger +35')
         elif action == "attack":
-            joy = -100
-            hungry = -50
+            joy += -100
+            hungry += -50
+            print('Boomy is mad: happiness -100, hunger -50')
         elif action == "pet":
-            joy= 30
-            hungry =-10
+            joy += 30
+            hungry +=-10
+            print("Boomy is happier: happiness +10, hunger -10")
     
         self.happiness += joy
         self.hunger += hungry
@@ -38,7 +42,8 @@ def happy(self):
             break 
         
         
-
+    print(f"Boomy's happiness is now", {self.happiness})
+    print(f"Boomy's hunger is now", {self.hunger})
     if self.happiness <= 0 or self.hunger <= 0:
         print(self.name, "is dead")
     elif self.happiness >= 0 and self.happiness <= 50: 
